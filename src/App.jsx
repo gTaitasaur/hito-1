e7fa0b16f61ad4383d5d096948170354ca99c195
gtaitasaur <gtaitasaur@gmail.com>
2024-11-02 21:22:26 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
